5149fd0a578c73cd96355faf9dd398f2c8ec8a04
Nick Lastovenko <bannerlog@gmail.com>
2017-12-04 00:31:31 +0300

    Updated documentation of the protocol

--- protocol/doc.go
+++ protocol/doc.go
@@ -23,5 +23,30 @@ Server doesn't close a connection after response was sent. It keeps connection
 open until the client won't close it. So you can send as many request as you want.
 That is just a test application, so no limits except hardware.
 
+Rsponse message structure looks like this. As in the request part above,
+the response must have CRLF at the end of each line.
+
+  Response type (could be OK or ERR)
+  Number of parts
+  Part length
+  Response part
+  ...
+
+Example of OK response.
+
+  OK\r\n
+  1\r\n
+  18\r\n
+  A hash field 1\r\n
+  32\r\n
+  Second value of a hash field\r\n
+
+Example of ERR response.
+
+  ERR\r\n
+  1\r\n
+  29\r\n
+  Wrong number of arguments\r\n
+
 */
 package redislike
